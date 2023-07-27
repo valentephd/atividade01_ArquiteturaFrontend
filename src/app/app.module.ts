20eb5c4349720e1da11d0097f03344d520ecc0ba
Wilson Valente <wilsonv@ciandt.com>
2023-07-26 21:41:56 -0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { HeaderApresentationComponent } from './header-apresentation/header-apresentation.component';
+import { ContentBlogComponent } from './content-blog/content-blog.component';
+import { ContentArticleComponent } from './content-article/content-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    HeaderApresentationComponent,
+    ContentBlogComponent,
+    ContentArticleComponent
   ],
   imports: [
     BrowserModule
