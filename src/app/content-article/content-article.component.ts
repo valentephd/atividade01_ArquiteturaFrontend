0b265205adb3fdf1f4112da5876d1980a6ae56a2
Wilson Valente <wilsonv@ciandt.com>
2023-07-27 18:38:06 -0300

    Finish Activity

--- src/app/content-article/content-article.component.ts
+++ src/app/content-article/content-article.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-article.component.css']
 })
 export class ContentArticleComponent {
-
+  articles = [
+    {
+      title: "5 dicas para sua carreira profissional 1",
+      previewText: "Confira algumas dicas que podem alavancar sua carreira como desenvolvedor frontend"
+    },
+    {
+      title: "5 dicas para sua carreira profissional 2",
+      previewText: "Confira algumas dicas que podem alavancar sua carreira como desenvolvedor frontend"
+    },
+    {
+      title: "5 dicas para sua carreira profissional 3",
+      previewText: "Confira algumas dicas que podem alavancar sua carreira como desenvolvedor frontend"
+    }
+  ];
 }
